6f2bd7fff718ab6a473186eae17145d6e0583c30
yinchuandong <yincd520@sina.com>
2015-10-24 22:04:31 +0800

    准备增加redis

---
+++ src/com/yin/spell/RedisUtil.java
@@ -0,0 +1,33 @@
+package com.yin.spell;
+
+import java.io.File;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisUtil {
+
+static Jedis jedis = null;
+
+	
+	private RedisUtil(){
+		
+	}
+	
+	public static Jedis getInstance(){
+		return getInstance("127.0.0.1", 6379);
+	}
+	
+	public static Jedis getInstance(String ip, int port){
+		if(jedis == null){
+			jedis = new Jedis(ip, port);
+		}
+		return jedis;
+	}
+	
+	public static void close(){
+		if(jedis != null){
+			jedis.disconnect();
+			jedis = null;
+		}
+	}
+}
